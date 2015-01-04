bc40f85cd53250defd5895ba5807ba6bb7bccc4d
HillLiu <s843098@tpts5.seed.net.tw>
2015-01-05 00:50:50 +0800

    wip

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ react-atomic-ui
 
 Demo Url:
 https://dl.dropboxusercontent.com/u/3396570/list/index.html
+
+Documentation:
+
--- src/myApp.js
+++ src/myApp.js
@@ -2,17 +2,10 @@ var React = require('react');
 
 var App = React.createFactory(require('./doc/index'));
 
-
-// This is just a simple example of a component that can be rendered on both
-// the server and browser
-
-var DocumentationApplicationView = React.createClass({
-
-
+module.exports = React.createClass({
+  displayName: 'MyApp',
   // For ease of illustration, we just use the React JS methods directly
   // (no JSX compilation needed)
-  // Note that we allow the button to be disabled initially, and then enable it
-  // when everything has loaded
   render: function() {
     return (
      App({ prop: 'value' })
@@ -20,5 +13,3 @@ var DocumentationApplicationView = React.createClass({
   },
 });
 
-
-module.exports = DocumentationApplicationView;
