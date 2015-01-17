dd97500df9853a0629a7e41e277fca9c741a1af8
Hill <hroohill@yahoo-inc.com>
2015-01-17 20:11:04 +0000

    add react style mixin

---
+++ ui/mixins/styles/Style.js
@@ -0,0 +1,17 @@
+/*jslint browser: true*/
+'use strict';
+var Store                  = require('./store.js');
+
+function Style(style, className, selector) {
+  this.style = style;
+  this.className = className;
+  this.selector = selector;
+}
+
+
+Style.prototype.isCompiled = function() {
+    var registry = Store.styles;
+    return registry && registry[this.className];
+};
+
+module.exports = Style;
