c480f0212ccbe9cf1624cb2a62713f0cfcf94b94
HillLiu <hill@kimo.com>
2015-01-21 00:20:34 +0800

    remove backup file

--- ui/atoms.bak/h3.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-module.exports = React.createClass({
-  displayName: 'H3',
-  render: function() {
-    return (
-      <h3 {...this.props}>{this.props.children}</h3>
-    );
-  }
-});
--- ui/atoms.bak/h4.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-module.exports = React.createClass({
-  displayName: 'H4',
-  render: function() {
-    return (
-      <h4 {...this.props}>{this.props.children}</h4>
-    );
-  }
-});
