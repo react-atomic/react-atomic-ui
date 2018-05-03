8c920535bae9c933b8d38d3101e991664b9f3379
Hill <hill@kimo.com>
2018-05-03 08:59:19 +0000

    update video example

--- ui/organisms/YouTubeRWD.example.jsx
+++ ui/organisms/YouTubeRWD.example.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import {YoutubeRWD} from 'organism-react-video';
 
 const YouTubeRWDExample = () =>
-<YoutubeRWD videoId="_sQSXwdtxlY" />
+<YoutubeRWD videoId="_2i6CmKjN3E" />
 
 export default YouTubeRWDExample;
