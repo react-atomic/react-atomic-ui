ef10f4318248741749016f36354a5a2c3a525542
Hill <hill@kimo.com>
2018-09-06 18:13:31 +0000

    Add barchart

--- ui/pages/Icons.jsx
+++ ui/pages/Icons.jsx
@@ -1,7 +1,8 @@
-import React from 'react'; 
+import React from 'react';
 
 import Account from 'ricon/Account';
 import AccessTime from 'ricon/AccessTime';
+import BarChart from 'ricon/BarChart';
 import Build from 'ricon/Build';
 import Calendar from 'ricon/Calendar';
 import ChevronRight from 'ricon/ChevronRight';
@@ -58,125 +59,123 @@ import Warning from 'ricon/Warning';
 import {reactStyle} from 'react-atomic-molecule';
 
 import BasePage from '../pages/BasePage';
-import Doc from '../templates/Doc'; 
-
+import Doc from '../templates/Doc';
 
 const icons = [
-    ['Account', <Account />],
-    ['Account (circle)', <Account type="circle" />],
-    ['Account (supervisor)', <Account type="supervisor" />],
-    ['AccessTime', <AccessTime />],
-    ['AccessTime (fill)', <AccessTime type="fill" />],
-    ['AccessTime (update)', <AccessTime type="update" />],
-    ['Build', <Build />],
-    ['Calendar', <Calendar />],
-    ['ChevronRight', <ChevronRight />],
-    ['Code', <Code />],
-    ['CircleLoading', <CircleLoading />],
-    ['Dashboard', <Dashboard />],
-    ['Delete', <Delete />],
-    ['Delete (forever)', <Delete type="forever" />],
-    ['Delete (sweep)', <Delete type="sweep" />],
-    ['Download', <Download />],
-    ['Download (file)', <Download type="file" />],
-    ['Dropdown', <Dropdown />],
-    ['Dropdown (up)', <Dropdown type="up" />],
-    ['Dropdown (right)', <Dropdown type="right" />],
-    ['Dropdown (left)', <Dropdown type="left" />],
-    ['Description', <Description />],
-    ['Edit', <Edit />],
-    ['Email', <Email />],
-    ['Exit', <Exit />],
-    ['File', <File>.js</File>],
-    ['Facebook', <Facebook />],
-    ['FacebookMessage', <FacebookMessage />],
-    ['FeaturedPlayList', <FeaturedPlayList />],
-    ['Git', <Git />],
-    ['Help', <Help />],
-    ['Home', <Home />],
-    ['Hamburger', <Hamburger />],
-    ['HamburgerToArrow', <HamburgerToArrow on={true} />],
-    ['HamburgerToX', <HamburgerToX on={true} />],
-    ['Link', <Link />],
-    ['Monetization', <Monetization />],
-    ['Monetization', <Monetization type="sign" />],
-    ['MyLocation (fixed)', <MyLocation />],
-    ['MyLocation (off)', <MyLocation type="off" />],
-    ['MyLocation (on)', <MyLocation type="on" />],
-    ['MyLocation (loading)', <MyLocation loading={true} type="on" />],
-    ['Npm', <Npm />],
-    ['Notification', <Notification />],
-    ['Notification (active)', <Notification type="active" />],
-    ['Notification (none)', <Notification type="none" />],
-    ['Notification (off)', <Notification type="off" />],
-    ['Notification (paused)', <Notification type="paused" />],
-    ['Person', <Person />],
-    ['Person (add)', <Person type="add" />],
-    ['Phone', <Phone />],
-    ['Pin',<Pin />],
-    ['Pulse',<Pulse />],
-    ['Pulse (breath)',<Pulse animation="breath" />],
-    ['Play',<Play />],
-    ['PlayListAdd',<PlayListAdd />],
-    ['Refresh',<Refresh />],
-    ['Search', <Search />],
-    ['Settings', <Settings />],
-    ['Settings', <Settings type='square' />],
-    ['ShoppingCart', <ShoppingCart />],
-    ['Sort', <Sort />],
-    ['Shopee', <Shopee />],
-    ['Sync', <Sync />],
-    ['Sync (disabled)', <Sync type="disabled" />],
-    ['Sync (problem)', <Sync type="problem" />],
-    ['Timer', <Timer />],
-    ['Timer (off)', <Timer type="off" />],
-    ['VerifiedUser', <VerifiedUser />],
-    ['Video', <Video />],
-    ['Wifi', <Wifi />],
-    ['Wallet', <Wallet />],
-    ['X', <X style={{position:'static'}}/>],
-    ['ZoomIn', <ZoomIn />],
-// Alert
-    ['Error', <Error />],
-    ['Warning', <Warning />],
+  ['Account', <Account />],
+  ['Account (circle)', <Account type="circle" />],
+  ['Account (supervisor)', <Account type="supervisor" />],
+  ['AccessTime', <AccessTime />],
+  ['AccessTime (fill)', <AccessTime type="fill" />],
+  ['AccessTime (update)', <AccessTime type="update" />],
+  ['BarChart', <BarChart />],
+  ['Build', <Build />],
+  ['Calendar', <Calendar />],
+  ['ChevronRight', <ChevronRight />],
+  ['Code', <Code />],
+  ['CircleLoading', <CircleLoading />],
+  ['Dashboard', <Dashboard />],
+  ['Delete', <Delete />],
+  ['Delete (forever)', <Delete type="forever" />],
+  ['Delete (sweep)', <Delete type="sweep" />],
+  ['Download', <Download />],
+  ['Download (file)', <Download type="file" />],
+  ['Dropdown', <Dropdown />],
+  ['Dropdown (up)', <Dropdown type="up" />],
+  ['Dropdown (right)', <Dropdown type="right" />],
+  ['Dropdown (left)', <Dropdown type="left" />],
+  ['Description', <Description />],
+  ['Edit', <Edit />],
+  ['Email', <Email />],
+  ['Exit', <Exit />],
+  ['File', <File>.js</File>],
+  ['Facebook', <Facebook />],
+  ['FacebookMessage', <FacebookMessage />],
+  ['FeaturedPlayList', <FeaturedPlayList />],
+  ['Git', <Git />],
+  ['Help', <Help />],
+  ['Home', <Home />],
+  ['Hamburger', <Hamburger />],
+  ['HamburgerToArrow', <HamburgerToArrow on={true} />],
+  ['HamburgerToX', <HamburgerToX on={true} />],
+  ['Link', <Link />],
+  ['Monetization', <Monetization />],
+  ['Monetization', <Monetization type="sign" />],
+  ['MyLocation (fixed)', <MyLocation />],
+  ['MyLocation (off)', <MyLocation type="off" />],
+  ['MyLocation (on)', <MyLocation type="on" />],
+  ['MyLocation (loading)', <MyLocation loading={true} type="on" />],
+  ['Npm', <Npm />],
+  ['Notification', <Notification />],
+  ['Notification (active)', <Notification type="active" />],
+  ['Notification (none)', <Notification type="none" />],
+  ['Notification (off)', <Notification type="off" />],
+  ['Notification (paused)', <Notification type="paused" />],
+  ['Person', <Person />],
+  ['Person (add)', <Person type="add" />],
+  ['Phone', <Phone />],
+  ['Pin', <Pin />],
+  ['Pulse', <Pulse />],
+  ['Pulse (breath)', <Pulse animation="breath" />],
+  ['Play', <Play />],
+  ['PlayListAdd', <PlayListAdd />],
+  ['Refresh', <Refresh />],
+  ['Search', <Search />],
+  ['Settings', <Settings />],
+  ['Settings', <Settings type="square" />],
+  ['ShoppingCart', <ShoppingCart />],
+  ['Sort', <Sort />],
+  ['Shopee', <Shopee />],
+  ['Sync', <Sync />],
+  ['Sync (disabled)', <Sync type="disabled" />],
+  ['Sync (problem)', <Sync type="problem" />],
+  ['Timer', <Timer />],
+  ['Timer (off)', <Timer type="off" />],
+  ['VerifiedUser', <VerifiedUser />],
+  ['Video', <Video />],
+  ['Wifi', <Wifi />],
+  ['Wallet', <Wallet />],
+  ['X', <X style={{position: 'static'}} />],
+  ['ZoomIn', <ZoomIn />],
+  // Alert
+  ['Error', <Error />],
+  ['Warning', <Warning />],
 ];
 
-class Icons extends BasePage 
-{
-    static defaultProps = {
-        pageName: 'icons' 
-    };
-    render()
-    {
-        return (
-            <Doc>
-            <div className="pure-g">
-            {
-                icons.map((v,k)=>
-                    <div key={k} className="pure-u-1 pure-u-md-1-3 pure-u-lg-1-5" style={Styles.grid}>
-                        <div style={Styles.icon}>{v[1]}</div>
-                        <div>{v[0]}</div>
-                    </div>
-                )
-            }
+class Icons extends BasePage {
+  static defaultProps = {
+    pageName: 'icons',
+  };
+  render() {
+    return (
+      <Doc>
+        <div className="pure-g">
+          {icons.map((v, k) => (
+            <div
+              key={k}
+              className="pure-u-1 pure-u-md-1-3 pure-u-lg-1-5"
+              style={Styles.grid}>
+              <div style={Styles.icon}>{v[1]}</div>
+              <div>{v[0]}</div>
             </div>
-            </Doc>
-        );
-    }
+          ))}
+        </div>
+      </Doc>
+    );
+  }
 }
 
 export default Icons;
 
 const Styles = {
-    grid: {
-        textAlign: 'center'
-    },
-    icon: {
-        maxWidth: 30,
-        minHeight: 30,
-        maxHeight: 30,
-        margin: '0 auto 10px',
-        position: 'relative',
-    }
+  grid: {
+    textAlign: 'center',
+  },
+  icon: {
+    maxWidth: 30,
+    minHeight: 30,
+    maxHeight: 30,
+    margin: '0 auto 10px',
+    position: 'relative',
+  },
 };
-
