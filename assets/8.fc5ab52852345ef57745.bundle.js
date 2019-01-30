c48243f4e7c9b323f3086b38538845d87df42568
Hill <hill@kimo.com>
2019-01-30 18:42:36 +0000

    improve animate

---
+++ assets/8.fc5ab52852345ef57745.bundle.js
@@ -0,0 +1,28 @@
+webpackJsonp([8],{
+
+/***/ 1152:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var css = {
+  fadeOutRight: [[{
+    opacity: 1
+  }, {
+    opacity: 0,
+    transform: ['translate3d(100%, 0, 0)']
+  }], ['@keyframes fadeOutRight', 'from', 'to']]
+};
+var _default = css;
+exports.default = _default;
+module.exports = exports.default;
+
+/***/ })
+
+});
+//# sourceMappingURL=8.fc5ab52852345ef57745.bundle.js.map
