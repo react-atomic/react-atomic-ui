d9a04f4d001c47bc9b24be28d1202f36e098e569
Hill <hill@kimo.com>
2022-04-05 10:41:47 +0800

    init react18

--- ui/pages/__tests__/AtomsTest.js
+++ ui/pages/__tests__/AtomsTest.js
@@ -1,12 +1,12 @@
 import { expect } from "chai";
-import { mount } from "reshow-unit";
+import { render } from "reshow-unit";
 
 import Atoms from "../Atoms";
 
 describe("Atoms page Test", () => {
   it("basic test", () => {
     const vDom = <Atoms />;
-    const wrap = mount(vDom);
+    const wrap = render(vDom);
     const html = wrap.html();
     expect(html).to.have.string("This is atom");
   });
