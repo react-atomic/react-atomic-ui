96ab693fa376e1aa87dcabc05db41ce167476479
Hill <hill@kimo.com>
2022-04-06 23:06:23 +0800

    test swc

---
+++ assets/11.e9c09bc17f8de2b9ad13.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[11],{266(b,a){Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0,a.default={fadeInDown:[[{opacity:0,transform:["translate3d(0, -100%, 0)"]},{opacity:1,transform:["none"]}],["@keyframes fadeInDown","from","to"]]},b.exports=a.default}}])
---
+++ assets/12.e9c09bc17f8de2b9ad13.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[12],{267(b,a){Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0,a.default={fadeInLeft:[[{opacity:0,transform:["translate3d(-100%, 0, 0)"]},{opacity:1,transform:["none"]}],["@keyframes fadeInLeft","from","to"]]},b.exports=a.default}}])
