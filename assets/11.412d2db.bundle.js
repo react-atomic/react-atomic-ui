43f5bb9c591d5fed90062225bce44245b4b88b72
Hill <hill@kimo.com>
2022-06-17 17:47:02 +0800

    improve easter egg

--- assets/11.412d2db.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[11],{230(c,a,b){b.r(a),b.d(a,{default:()=>d});let d={fadeInUp:[[{opacity:0,transform:["translate3d(0, 100%, 0)"]},{opacity:1,transform:["none"]}],["@keyframes fadeInUp","from","to"]]}}}])
--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DIR=$( cd "$(dirname "$0")" ; pwd -P )
+DIR=$(
+  cd "$(dirname "$0")"
+  pwd -P
+)
 cd $DIR
 SWJS=${DIR}/service-worker.js
 
@@ -14,8 +17,8 @@ conf+='"swDest":"'${SWJS}'",'
 conf+='"hotPort": "'${hotPort:-3088}'"'
 conf+='}'
 
-OPEN=$(which xdg-open 2>/dev/null)
-if [ -z "$OPEN" ]; then 
+OPEN=$(which xdg-open 2> /dev/null)
+if [ -z "$OPEN" ]; then
   OPEN="open"
 fi
 
@@ -23,7 +26,7 @@ if [ "xon" == "xon" ]; then
   webpack='npm run webpack --'
 fi
 
-checkBabel(){
+checkBabel() {
   if [ ! -e ".babelrc" ] && [ ! -e "../../packages" ]; then
     if [ -e ${DIR}/node_modules/reshow-app/.babelrc ]; then
       cp ${DIR}/node_modules/reshow-app/.babelrc ${DIR}/.babelrc
@@ -31,91 +34,96 @@ checkBabel(){
   fi
 }
 
-killBy(){
-    ps -eo pid,args | grep $1 | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
+killBy() {
+  ps -eo pid,args | grep $1 | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
 }
 
-stop(){
-    killBy ${DIR}/node_modules/.bin/babel 
-    cat webpack.pid | xargs -I{} kill -9 {}
-    npm run clean
-    rm $SWJS
-    echo "Stop done";
+stop() {
+  killBy ${DIR}/node_modules/.bin/babel
+  cat webpack.pid | xargs -I{} kill -9 {}
+  npm run clean
+  rm $SWJS
+  echo "Stop done"
 }
 
-stopServer(){
+stopServer() {
   killBy ${DIR}/node_modules/.bin/ws
-  echo "stop server done";
+  echo "stop server done"
 }
 
-startServer(){
-    stopServer
-    yarn
-    if [ ! -e "build" ]; then
-        develop
-    fi
-    port=${port-3000}
-    echo "Start server";
-    if [ "$1" == "open" ]; then
-        npm run start -- -p $port &
-        sleep 3 
-        $OPEN http://localhost:$port        
-    else
-        npm run start -- -p $port -v
-    fi
+startServer() {
+  stopServer
+  yarn
+  if [ ! -e "build" ]; then
+    develop
+  fi
+  port=${port-3000}
+  echo "Start server"
+  if [ "$1" == "open" ]; then
+    npm run start -- -p $port &
+    sleep 3
+    $OPEN http://localhost:$port
+  else
+    npm run start -- -p $port -v
+  fi
 }
 
-production(){
-    stop
-    echo "Production Mode";
-    checkBabel
-    npm run build
-    ENABLE_SW=1 CONFIG=$conf NODE_ENV=production $webpack
+production() {
+  stop
+  echo "Production Mode"
+  checkBabel
+  npm run build
+  npm run clean:webpack
+  ENABLE_SW=1 CONFIG=$conf NODE_ENV=production $webpack
 }
 
-analyzer(){
-    stop
-    echo "Analyzer Mode";
-    checkBabel
-    npm run build
-    CONFIG=$conf BUNDLE='{}' $webpack
+analyzer() {
+  stop
+  echo "Analyzer Mode"
+  checkBabel
+  npm run build
+  npm run clean:webpack
+  CONFIG=$conf BUNDLE='{}' $webpack
 }
 
-develop(){
-    stop
-    echo "Develop Mode";
-    checkBabel
-    npm run build
-    CONFIG=$conf $webpack
+develop() {
+  stop
+  echo "Develop Mode"
+  checkBabel
+  npm run build
+  npm run clean:webpack
+  CONFIG=$conf $webpack
 }
 
-watch(){
-    stop 
-    echo "Watch Mode";
-    checkBabel
-    npm run build:es:ui -- --watch &
-    npm run build:es:src -- --watch &
-    sleep 10 
-    CONFIG=$conf $webpack --watch &
+watch() {
+  stop
+  echo "Watch Mode"
+  checkBabel
+  npm run build:es:ui -- --watch &
+  npm run build:es:src -- --watch &
+  sleep 10
+  npm run clean:webpack
+  CONFIG=$conf $webpack --watch &
 }
 
-watchTest(){
-    stop 
-    echo "Watch Test";
-    checkBabel
-    npm run build:cjs:ui -- --watch &
-    npm run build:cjs:src -- --watch &
+watchTest() {
+  stop
+  echo "Watch Test"
+  checkBabel
+  npm run build:cjs:ui -- --watch &
+  npm run build:cjs:src -- --watch &
 }
 
-hot(){
-    stop
-    rm $SWJS 
-    echo "Hot Mode";
-    checkBabel
-    npm run build:es:ui -- --watch &
-    npm run build:es:src -- --watch &
-    sleep 5
-    HOT_UPDATE=1 CONFIG=$conf $webpack serve &
+hot() {
+  stop
+  rm $SWJS
+  echo "Hot Mode"
+  checkBabel
+  npm run build:es:ui -- --watch &
+  npm run build:es:src -- --watch &
+  sleep 10
+  npm run clean:webpack
+  HOT_UPDATE=1 CONFIG=$conf $webpack serve &
 }
 
 case "$1" in
@@ -123,7 +131,7 @@ case "$1" in
     production
     ;;
   a)
-    analyzer 
+    analyzer
     ;;
   s)
     startServer $2
@@ -135,17 +143,18 @@ case "$1" in
     hot
     ;;
   watch)
-    watch 
+    watch
     ;;
   watchTest)
     watchTest
     ;;
   stop)
-    stop 
+    stop
     ;;
   *)
     develop
     exit
+    ;;
 esac
 
 exit $?
