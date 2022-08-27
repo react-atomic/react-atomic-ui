7878fece0467e61187ca1e95fec4c49c61ecec16
Hill <hill@kimo.com>
2022-08-27 19:21:33 +0800

    update new version

---
+++ assets/31.f7447a9.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[31],{363(c,a,b){function d(){return new Worker(b.p+"worker.bundle.worker.js")}b.r(a),b.d(a,{default:()=>d})}}])
--- src/ui/organisms/TagInput.example.jsx
+++ src/ui/organisms/TagInput.example.jsx
@@ -6,16 +6,23 @@ import { Checkbox } from "react-atomic-organism";
 const TagInputExample = (props) => {
   const [isDisable, setIsDisable] = useState(false);
   const [limit, setLimit] = useState(-1);
+  const [couldDuplicate, setCouldDuplicate] = useState();
 
   const handleDisable = (e) => {
     setIsDisable(e.checked ? true : false);
   };
+  const handleCouldDuplicate= (e) => {
+    setCouldDuplicate(e.checked ? true : false);
+  };
 
   return (
     <Form>
-      <TagsField disabled={isDisable} maxTags={limit} />
+      <TagsField disabled={isDisable} maxTags={limit} couldDuplicate={couldDuplicate}/>
       <Field>
         <Checkbox label="Disable" toggle onChange={handleDisable} />
+      </Field>
+      <Field>
+        <Checkbox label="Could Duplicate" toggle onChange={handleCouldDuplicate} />
       </Field>
       <Field label="Limit tags" atom="input" type="number" inline defaultValue="-1" onChange={(e)=>{setLimit(e.currentTarget.value)}} min="-1"/>
     </Form>
