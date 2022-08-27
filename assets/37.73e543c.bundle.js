e82092d7be62f80c985a72c288c77e4298177e09
Hill <hill@kimo.com>
2022-08-27 13:33:47 +0800

    update js

--- assets/37.73e543c.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[37],{376(c,a,b){b.r(a),b.d(a,{default:()=>d});let d={fadeInRight:[[{opacity:0,transform:["translate3d(100%, 0, 0)"]},{opacity:1,transform:["none"]}],["@keyframes fadeInRight","from","to"]]}}}])
