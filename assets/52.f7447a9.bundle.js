1c7cd318099dfec5ec2ceb36455780a01468257f
Hill <hill@kimo.com>
2022-08-28 15:38:00 +0800

    Let Command Palette adaptive width

--- assets/52.f7447a9.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[52],{466(c,a,b){b.r(a),b.d(a,{default:()=>d});let d={fadeOutLeft:[[{opacity:1},{opacity:0,transform:["translate3d(-100%, 0, 0)"]}],["@keyframes fadeOutLeft","from","to"]]}}}])
