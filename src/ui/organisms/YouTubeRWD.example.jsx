f5a751f148ca20866d918070031231da7665c65d
Hill <hill@kimo.com>
2022-09-03 01:07:44 +0800

    Add enable controls

--- src/ui/organisms/YouTubeRWD.example.jsx
+++ src/ui/organisms/YouTubeRWD.example.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import { YoutubeRWD } from "organism-react-video";
+import { Checkbox } from "react-atomic-organism";
 
-const YouTubeRWDExample = () => <YoutubeRWD videoId="CjxugyZCfuw" />;
+const YouTubeRWDExample = () => {
+  const [controls, setControls] = useState(1);
+  const handleControls = (e) => setControls(e.checked ? 1 : 0);
+  return (
+    <>
+      <Checkbox label="Enable controls" toggle onChange={handleControls} checked={controls} />
+      <YoutubeRWD videoId="CjxugyZCfuw" videoParams={{ controls }} mask={false} showControllBar={controls} />
+    </>
+  );
+};
 
 export default YouTubeRWDExample;
